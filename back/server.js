fff8aac4a0154669fb6fd98da7ba15f2eb95698a
frangoni <fran.goni96@gmail.com>
2020-12-03 15:25:22 -0300

    littlebits

--- back/server.js
+++ back/server.js
@@ -1,7 +1,4 @@
 const express = require("express");
-// const app = express();
-// const server = require("http").Server(app) //es necesario para usar websockets armar un server http y pasarle la app de express
-// const io = require("socket.io")(server)
 const { server, app } = require("./io");
 
 const volleyball = require("volleyball");
