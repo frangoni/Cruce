e4a65021ea21ec889d4fc923af6ba34263df953a
Lucas <lucasnovaroh@gmail.com>
2020-12-04 14:49:03 -0300

    version demo 2

--- back/api/db/index.js
+++ back/api/db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://postgres:gustihero@localhost:5433/cruce", {
+const db = new Sequelize("postgres://postgres:novarohueyo@localhost:5432/cruce", {
   dialect: "postgres",
   host: "localhost",
   logging: false,
--- front/src/components/websocket.js
+++ front/src/components/websocket.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import OrdersTable from "./OrderTable";
 import io from "socket.io-client";
 import { useSelector, useDispatch } from "react-redux";
-<<<<<<< HEAD
 import { fetchOrders, addOrders, filterOrders, fetchPickOrder, updateOrder } from "../redux/actions/orders"
 import SheetUpload from "./SheetUpload";
 
@@ -10,26 +9,12 @@ const WebSocket = () => {
   const dispatch = useDispatch()
   const { orders } = useSelector(state => state.orders)
   const { role, id } = useSelector(state => state.user.user)
-=======
-import {
-  fetchOrders,
-  addOrders,
-  filterOrders,
-  fetchPickOrder,
-} from "../redux/actions/orders";
-import SheetUpload from "./SheetUpload";
-
-const WebSocket = () => {
-  const dispatch = useDispatch();
-  const { orders } = useSelector((state) => state.orders);
->>>>>>> 6fd64f50c3dcab889e73ccfa72afc23dc46de818
 
   const handler = (orderId) => {
     dispatch(fetchPickOrder(orderId));
   };
 
   useEffect(() => {
-<<<<<<< HEAD
     dispatch(fetchOrders())
     const socket = io.connect(`${window.location.origin}`, { query: { role } });
 
@@ -43,18 +28,6 @@ const WebSocket = () => {
       const order = JSON.parse(data)
       if (role === "Cadete") dispatch(filterOrders(order.orderId))
       else if (role === "Empresa") dispatch(updateOrder(order))
-=======
-    dispatch(fetchOrders());
-    const socket = io.connect(`${window.location.origin}`, { forceNew: true });
-
-    socket.on("ordersCreated", (data) => {
-      dispatch(addOrders([...JSON.parse(data)]));
-    });
-
-    socket.on("dbModifications", (data) => {
-      const orderId = JSON.parse(data).orderId;
-      dispatch(filterOrders(orderId));
->>>>>>> 6fd64f50c3dcab889e73ccfa72afc23dc46de818
     });
 
     return () => {
