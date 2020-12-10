a28fb09d7bdb769cad3f26331d811832df752fae
gustavo <gustavo4111997@gmail.com>
2020-12-10 15:43:40 -0300

    logout

--- front/src/components/splash.js
+++ front/src/components/splash.js
@@ -1,31 +1,27 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 
 const Splash = () => {
   return (
     <div className="container">
-      <div className="imagenes">
-        <img className="logo" src={"./arbol.png"} />
-        <img className="logo2" src={"./cruce.png"} />
-        <div className="buttons">
-          <Link to="/login">
-            <Button variant="outlined" color="primary">
-              <p>Ingresar</p>
-            </Button>
-          </Link>
-          <Link to="/register">
-            <Button variant="outlined" color="primary">
-              <p>Registrarse</p>
-            </Button>
-          </Link>
-        </div>
+      <div className="logo">
+        <img className="logo2" src={"./cruce.svg"} />
       </div>
-      <div className="barco">
-        <div className="barco2">
-          <img src={"./barca.png"}></img>
-        </div>
+      <div className="buttons">
+        <Link className="boton" to="/login">
+          <Button variant="contained" color="primary">
+            Ingresar
+          </Button>
+        </Link>
+        <Link className="boton2" to="/register">
+          <Button variant="contained" color="primary">
+            Registrarse
+          </Button>
+        </Link>
+      </div>
+      <div className="barco2">
+        <img src={"./barca.png"}></img>
       </div>
     </div>
   );
--- front/src/components/websocket.js
+++ front/src/components/websocket.js
@@ -1,33 +1,48 @@
 import React, { useEffect } from "react";
 import OrdersTable from "./OrderTable";
 import io from "socket.io-client";
+import Button from "@material-ui/core/Button";
+
 import { useSelector, useDispatch } from "react-redux";
-import { fetchOrders, addOrders, filterOrders, fetchPickOrder, updateOrder } from "../redux/actions/orders"
+import {
+  fetchOrders,
+  addOrders,
+  filterOrders,
+  fetchPickOrder,
+  updateOrder,
+} from "../redux/actions/orders";
 import SheetUpload from "./SheetUpload";
-
+import { userLogout } from "../redux/actions/user";
+import { useHistory } from "react-router-dom";
 const WebSocket = () => {
-  const dispatch = useDispatch()
-  const { orders } = useSelector(state => state.orders)
-  const { role, id } = useSelector(state => state.user.user)
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { orders } = useSelector((state) => state.orders);
+  const { role, id } = useSelector((state) => state.user.user);
 
+  const setLogout = () => {
+    dispatch(userLogout());
+    history.push("/splash");
+  };
   const handler = (orderId) => {
     dispatch(fetchPickOrder(orderId));
   };
 
   useEffect(() => {
-    dispatch(fetchOrders())
+    dispatch(fetchOrders());
     const socket = io.connect(`${window.location.origin}`, { query: { id } });
 
-    socket.on('ordersCreated', (data) => {
-      const orders = JSON.parse(data)
-      console.log("orders del bulkcreate", orders)
-      if (role === "Cadete" || (orders.empresa === id)) dispatch(addOrders(orders.ordenes))
+    socket.on("ordersCreated", (data) => {
+      const orders = JSON.parse(data);
+      console.log("orders del bulkcreate", orders);
+      if (role === "Cadete" || orders.empresa === id)
+        dispatch(addOrders(orders.ordenes));
     });
 
-    socket.on('dbModifications', (data) => {
-      const order = JSON.parse(data)
-      if (role === "Cadete") dispatch(filterOrders(order.orderId))
-      else if (role === "Empresa") dispatch(updateOrder(order))
+    socket.on("dbModifications", (data) => {
+      const order = JSON.parse(data);
+      if (role === "Cadete") dispatch(filterOrders(order.orderId));
+      else if (role === "Empresa") dispatch(updateOrder(order));
     });
 
     return () => {
@@ -37,6 +52,9 @@ const WebSocket = () => {
 
   return (
     <>
+      <Button variant="contained" color="primary" onClick={setLogout}>
+        Logout
+      </Button>
       <SheetUpload />
       {orders.length ? (
         <div style={{ height: 800, width: "100%" }}>
