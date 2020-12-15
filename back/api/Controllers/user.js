53d3a15139d24efbac3d14b1b4ae4a5cf13129c0
frangoni <fran.goni96@gmail.com>
2020-12-15 12:02:25 -0300

    persistencia(borrar user y token si expira)

--- back/api/Controllers/user.js
+++ back/api/Controllers/user.js
@@ -27,25 +27,15 @@ const userValidation = async (req, res, next) => {
 const userCreation = async (req, res, next) => {
   try {
     const user = await User.create(req.body);
-    /*const cadeteria = await Cadeteria.findOrCreate({
-      where: { name: req.body.cadeteria },
-    });
-    cadeteria.addUser(user); */
     res.status(201).send(user);
   } catch (err) {
     res.status(400).send(err);
   }
 };
 
-/* const setCadeteria = async (req, res, next) => {
-  const cadeteria = await Cadeteria.findByPk(req.body.cadeteria);
-  const user = await User.findByPk(req.body.user);
-  cadeteria.addUser(user);
-}; */
-
 const userData = (req, res, next) => {
-  if (req.user) return res.send(req.user);
-  res.status(401).send({ error: "token invalido" });
+  if (req.user) return res.status(200).send(req.user);
+  res.status(401).send({ error: "Token invalido" });
 };
 
 module.exports = { userValidation, userCreation, userData };
