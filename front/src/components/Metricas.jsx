dec0226aeb5ac08a8c8370b9996ecbc7ff227af5
frangoni <fran.goni96@gmail.com>
2020-12-16 18:00:59 -0300

    filtros en grafico

--- front/src/components/Metricas.jsx
+++ front/src/components/Metricas.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Radio,
+  RadioGroup,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+} from "@material-ui/core";
 import { fetchMetricas } from "../redux/actions/metricas";
 import { Line } from "react-chartjs-2";
 
@@ -10,13 +17,38 @@ export default function SpacingGrid() {
   const dispatch = useDispatch();
   const { metricas, orders } = useSelector((state) => state.metricas);
   const [ejeY, setEjeY] = useState("delay");
-  const [text, setText] = useState("Promedio de entrega en minutos");
+  const [text, setText] = useState("Duración desde asignación a entrega");
+  const user = useSelector((state) => state.user.user);
+
+  const indices = [
+    //agregar objeto donde value sea igual al virtual del modelo Order
+    {
+      value: "delay",
+      label: "Duración desde asignación a entrega",
+    },
+    {
+      value: "delayTransport",
+      label: "Duración desde recogida a entrega",
+    },
+    {
+      value: "delayCircuit",
+      label: "Duración total",
+    },
+  ];
+
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    setEjeY(value);
+    indices.map((indice) => {
+      if (indice.value == value) setText(indice.label);
+    });
+  };
 
   const axis = (arr) => {
     const y = [];
     const x = [];
     for (let i = 0; i < arr.length; i++) {
-      y.push(orders[i][ejeY] / 60000);
+      y.push(orders[i][ejeY] / 60000); //para pasar a minutos(viene en milisegundos)
       x.push(orders[i].orderId);
     }
     return { x, y };
@@ -42,6 +74,10 @@ export default function SpacingGrid() {
 
   return (
     <>
+      <div style={{ margin: "1em 3em" }}>
+        <Typography variant="h3">Bienvenid@ {user.name}!</Typography>
+        <Typography variant="h6">Estas son tus métricas de rendimiento:</Typography>
+      </div>
       <div id="metricsContainer">
         <Card className="metriCards">
           <CardContent>
@@ -79,7 +115,7 @@ export default function SpacingGrid() {
           </CardContent>
         </Card>
       </div>
-      <div>
+      <div id="graphContainer">
         <Line
           id="chart"
           data={state}
@@ -95,6 +131,16 @@ export default function SpacingGrid() {
             },
           }}
         />
+        <span>
+          <FormControl component="fieldset">
+            <FormLabel component="legend"></FormLabel>
+            <RadioGroup aria-label="metricas" name="metricas" value={ejeY} onChange={handleChange}>
+              {indices.map((indice, id) => {
+                return <FormControlLabel id={id} value={indice.value} control={<Radio />} label={indice.label} />;
+              })}
+            </RadioGroup>
+          </FormControl>
+        </span>
       </div>
     </>
   );
